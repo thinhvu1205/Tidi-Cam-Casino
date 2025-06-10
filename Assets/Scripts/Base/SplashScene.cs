18f3d783ab328af0293b43f6f1cf4a8b01a52ee7
thinhvu1205 <thinhvu120593@gmail.com>
2025-06-10 10:32:06 +0700

    1

--- Assets/Scripts/Base/SplashScene.cs
+++ Assets/Scripts/Base/SplashScene.cs
@@ -5,17 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class SplashScene : MonoBehaviour
 {
-    //https://console.cloud.google.com/storage/browser/kh9;tab=objects?forceOnBucketsSortingFiltering=true&inv=1&invt=AbzYXw&project=myanmar-199404&prefix=&forceOnObjectsSortingFiltering=false
+    // https://console.cloud.google.com/storage/browser/kh9;tab=objects?forceOnBucketsSortingFiltering=true&inv=1&invt=AbzYXw&project=myanmar-199404&prefix=&forceOnObjectsSortingFiltering=false
     [SerializeField] private BundleDownloader m_BundleBD;
 
     private void Awake()
     {
-        Config.Bundle_URL = "D:/Unity projects/Tidi-Phil-Win777/Assets/AssetBundles";
-        //  Config.Bundle_URL = PlayerPrefs.GetString(BundleDownloader.STORED_BUNDLE_URL, ""); // https://storage.googleapis.com/kh9/AssetBundles/
-        StartCoroutine(loadAssets());
-        IEnumerator loadAssets()
+        // D:/Unity projects/Tidi-Phil-Win777/Assets/AssetBundles;
+        // https://storage.googleapis.com/kh9/AssetBundles/
+        string storedUrl = PlayerPrefs.GetString(BundleDownloader.STORED_BUNDLE_URL, "");
+        m_BundleBD.CheckAndDownloadAssets(storedUrl,
+            () =>
+            {
+                m_BundleBD.SetProgressText("Retrying ...");
+                StartCoroutine(retry());
+            },
+            () =>
+            {
+                SceneManager.LoadScene("MainScene");
+            });
+
+        IEnumerator retry()
         {
-            while (Config.Bundle_URL.Equals("")) yield return new WaitForSeconds(1f);
+            while (Config.Bundle_URL.Equals("")) yield return new WaitForSeconds(3f);
             m_BundleBD.CheckAndDownloadAssets(Config.Bundle_URL,
                 () =>
                 {
@@ -24,9 +35,8 @@ public class SplashScene : MonoBehaviour
                 () =>
                 {
                     SceneManager.LoadScene("MainScene");
-                    StopAllCoroutines();
                 });
-
         }
+
     }
 }
